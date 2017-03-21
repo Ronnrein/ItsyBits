c56ccfce69d58dcaeb5d56b63919f2a1cbde80e5
Ronny Reinhardtsen <ronny@ronnrein.com>
2017-03-21 19:40:57 +0100

    Added ajax override for animal actions

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
     $(".progressbar").progress();
     $(".tooltip").popup();
+    $(".animal-action").submit(doAnimalAction);
 });
+
+// Intercept submit and do it through ajax
+function doAnimalAction() {
+    var that = this;
+    $.post($(this).attr("action"), function () {
+
+        // Update the progressbar
+        console.log($(that).find(".progressbar").progress({
+            percent: 100
+        }).attr("data-content", "100%"));
+    });
+    return false;
+}
