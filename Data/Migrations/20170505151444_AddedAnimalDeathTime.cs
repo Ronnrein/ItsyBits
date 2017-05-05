026d08d4046f1112f689d99af77216f8e4dc9589
Ronny Reinhardtsen <ronny@ronnrein.com>
2017-05-05 17:55:44 +0200

    Changed to daily rewards, animal now runs away if unhappy, chance to recover

---
+++ Data/Migrations/20170505151444_AddedAnimalDeathTime.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ItsyBits.Migrations
+{
+    public partial class AddedAnimalDeathTime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeathTime",
+                table: "Animals",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeathTime",
+                table: "Animals");
+        }
+    }
+}
