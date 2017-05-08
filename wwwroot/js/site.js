61160dcee9d605e7db240be6fea2cdaece1561ce
Ronny Reinhardtsen <ronny@ronnrein.com>
2017-05-08 14:31:50 +0200

    Started improving town implementation

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
     // Semantic events
     $(".progressbar").progress();
---
+++ wwwroot/js/store.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    $("#canvas").town({
+        persistantClick: true,
+        clickHandler: function(building) {
+            if (building.plot === 0) {
+                building.click();
+                return;
+            }
+        }
+    });
+});
