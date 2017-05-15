344162dc064dbd6fa7234ad2f6277cf2a417bd7b
Ronny Reinhardtsen <ronny@ronnrein.com>
2017-05-15 23:52:42 +0200

    Seperated sql files into executable scripts

--- scripts/dev.sql
+++ scripts/dev.sql
@@ -1,16 +1,4 @@
 /*
-    Run this as root on your dev server before migrating
-    (This will remove existing databases, beware)
-*/
-DROP DATABASE IF EXISTS `itsybits`;
-DROP DATABASE IF EXISTS `hangfire`;
-CREATE DATABASE `itsybits`;
-CREATE DATABASE `hangfire`;
-CREATE USER IF NOT EXISTS `itsybits`@`localhost` IDENTIFIED BY 'itsybits';
-GRANT ALL ON itsybits.* TO `itsybits`@`localhost`;
-GRANT ALL ON hangfire.* TO `itsybits`@`localhost`;
-FLUSH PRIVILEGES;
-/*
     Now run 'dotnet ef database update' to migrate the tables
     Afterwards, run this as root on your dev server
     After this, you can start the server and login using username and password 'admin'
