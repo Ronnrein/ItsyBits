d84fbdd547db22de511fbb5db040f33ab9c7a078
Ronny Reinhardtsen <ronny@ronnrein.com>
2017-05-15 11:27:22 +0200

    Got touch working

--- wwwroot/js/town.js
+++ wwwroot/js/town.js
@@ -1,7 +1,7 @@
 ﻿$.fn.town = function(options) {
 
     // Options
-    var aspectRatio = 2;
+    var aspectRatio = isMobile() ? 0.75 : 2;
     var background = new Sprite("/images/town/background2.png", new Vector2(0, 0));
     var cursor = new Sprite("/images/town/cursor.png", new Vector2(-2000, -2000), null, "/images/town/cursorPointer.png");
     var buildings = [
@@ -118,7 +118,9 @@
         // Render cursor
         ctx.save();
         ctx.setTransform(1, 0, 0, 1, 0, 0);
-        cursor.render(delta);
+        if (!isMobile()) {
+            cursor.render(delta);
+        }
         ctx.restore();
     }
 
@@ -164,13 +166,16 @@
 
     // Gets called when mouse button is pushed down
     function mouseDown(e) {
+        e.stopPropagation();
+        e = addTouchOffset(e);
         lastMousePosition = new Vector2(e.offsetX, e.offsetY);
         dragStartPosition = ctx.transformedPoint(lastMousePosition.x, lastMousePosition.y);
         dragged = false;
     }
 
     // Gets called when mouse button is released
-    function mouseUp() {
+    function mouseUp(e) {
+        e.stopPropagation();
         dragStartPosition = null;
         if (!dragged) {
             $.each(buildings, function (i, building) {
@@ -187,6 +192,8 @@
 
     // Gets called when mouse moves
     function mouseMove(e) {
+        e.stopPropagation();
+        e = addTouchOffset(e);
         lastMousePosition = new Vector2(e.offsetX, e.offsetY);
         dragged = true;
         var point = ctx.transformedPoint(lastMousePosition.x, lastMousePosition.y);
@@ -213,7 +220,7 @@
 
     // Zooms given amount in our out
     function zoom(amount) {
-        var pt = ctx.transformedPoint(lastMousePosition.x, lastMousePosition.y);
+        var pt = isMobile() ? new Vector2(background.rect.width / 2, background.rect.height / 2) : ctx.transformedPoint(lastMousePosition.x, lastMousePosition.y);
         var factor = Math.pow(scaleFactor, amount);
         currentScale *= factor;
         if (currentScale <= maxScale && currentScale >= minScale) {
@@ -254,6 +261,20 @@
         if (dragStartPosition && changed) {
             dragStartPosition = ctx.transformedPoint(lastMousePosition.x, lastMousePosition.y);
         }
+    }
+
+    // Add touch offset to event
+    function addTouchOffset(e) {
+        if (e.offsetX === undefined) {
+            e.offsetX = e.originalEvent.touches[0].pageX - e.originalEvent.touches[0].target.offsetLeft;
+            e.offsetY = e.originalEvent.touches[0].pageY - e.originalEvent.touches[0].target.offsetTop;
+        }
+        return e;
+    }
+
+    // Whether or not the device is mobile or not
+    function isMobile() {
+        return /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0, 4));
     }
 
     // Default building click handler
