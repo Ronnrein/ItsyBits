ff45bcfe246508361a39f3f57e537ac456716380
Ronny Reinhardtsen <ronny@ronnrein.com>
2017-05-15 14:39:44 +0200

    Added town to store

--- wwwroot/js/town.js
+++ wwwroot/js/town.js
@@ -4,8 +4,8 @@
     var aspectRatio = isMobile() ? 0.75 : 2;
     var background = new Sprite("/images/town/background2.png", new Vector2(0, 0));
     var cursor = new Sprite("/images/town/cursor.png", new Vector2(-2000, -2000), null, "/images/town/cursorPointer.png");
-    var buildings = [
-        new Building(0, new Sprite("/images/town/store.png", new Vector2(1720, 480), null, "/images/town/storehover.png"), "/store")
+    var plots = [
+        new Plot(0, null, new Sprite("/images/town/store.png", new Vector2(1720, 480), null, "/images/town/storehover.png"), "/store")
     ];
     var sprites = [
         new Sprite("/images/misc/coin.png", new Vector2(400, 500), new Animation(10, 1, 10, 50)),
@@ -19,8 +19,7 @@
 
     // Set default settings
     var settings = $.extend({
-        clickHandler: handleBuildingClick,
-        persistantClick: false
+        clickHandler: handleBuildingClick
     }, options);
 
     // Variables
@@ -45,22 +44,22 @@
 
     // Load images
     getData(function () {
-        $.each(buildings, function (i, building) {
-            sprites.unshift(building.sprite);
+        $.each(plots, function(i, plot) {
+            sprites.unshift(plot.sprite);
         });
         sprites.unshift(background);
         sprites.push(cursor);
         sources = [];
-        $.each(sprites, function (i, sprite) {
+        $.each(sprites, function(i, sprite) {
             sources.push({ source: sprite.imagePath, target: sprite.image });
             if (sprite.hoverImagePath != null) {
                 sources.push({ source: sprite.hoverImagePath, target: sprite.hoverImage });
             }
         });
-        loadImages(function () {
+        loadImages(function() {
 
-            // Set building rect to image size
-            $.each(sprites, function (i, sprite) {
+            // Set sprite rect to image size
+            $.each(sprites, function(i, sprite) {
                 sprite.rect.width = sprite.image.width;
                 sprite.rect.height = sprite.image.height;
             });
@@ -83,7 +82,7 @@
         $.each(sprites, function(i, sprite) {
             sprite.hover = false;
         });
-        $.each(sprites, function (i, sprite) {
+        $.each(sprites, function(i, sprite) {
             if (sprite !== cursor) {
                 sprite.hover = sprite.rect.contains(ctx.transformedPoint(lastMousePosition.x, lastMousePosition.y));
                 if (sprite.hoverImagePath != null && sprite.hover) {
@@ -92,9 +91,9 @@
                 }
             }
         });
-        $.each(buildings, function (i, building) {
-            if (clickedPlot === building.plot) {
-                building.sprite.hover = true;
+        $.each(plots, function (i, plot) {
+            if (clickedPlot === plot.id) {
+                plot.sprite.hover = true;
             }
         });
         cursor.rect.x = lastMousePosition.x;
@@ -111,27 +110,29 @@
         ctx.rect(p1.x, p1.y, p2.x - p1.x, p2.y - p1.y);
         ctx.fillStyle = backgroundColor;
         ctx.fill();
-        $.each(sprites, function (i, sprite) {
+
+        $.each(sprites, function(i, sprite) {
             sprite.render(delta);
         });
 
         // Render cursor
-        ctx.save();
-        ctx.setTransform(1, 0, 0, 1, 0, 0);
         if (!isMobile()) {
+            ctx.save();
+            ctx.setTransform(1, 0, 0, 1, 0, 0);
             cursor.render(delta);
+            ctx.restore();
         }
-        ctx.restore();
     }
 
     // Function to get user data
     function getData(callback) {
-        $.getJSON("/building/plots", function (data) {
-            $.each(data, function (i, v) {
+        $.getJSON("/building/plots", function(data) {
+            $.each(data, function(i, v) {
                 var image = "/images/" + (v.buildings == null ? "town/empty" : "buildings/" + v.buildings[0].type.spritePath + "/town");
                 var url = v.buildings == null ? "/store" : "/building/details/" + v.buildings[0].id;
-                buildings.push(new Building(
+                plots.push(new Plot(
                     v.id,
+                    v.buildings == null ? null : v.buildings[0],
                     new Sprite(
                         image + ".png",
                         new Vector2(v.positionX, v.positionY),
@@ -155,7 +156,7 @@
     function loadImages(callback) {
         var loaded = 0;
         for (var i in sources) {
-            sources[i].target.onload = function () {
+            sources[i].target.onload = function() {
                 if (++loaded >= sources.length) {
                     callback();
                 }
@@ -178,12 +179,12 @@
         e.stopPropagation();
         dragStartPosition = null;
         if (!dragged) {
-            $.each(buildings, function (i, building) {
-                if (building.sprite.rect.contains(ctx.transformedPoint(lastMousePosition.x, lastMousePosition.y))) {
-                    if (building.plot !== 0) {
-                        clickedPlot = building.plot;
+            $.each(plots, function (i, plot) {
+                if (plot.sprite.rect.contains(ctx.transformedPoint(lastMousePosition.x, lastMousePosition.y))) {
+                    if (plot.id !== 0) {
+                        clickedPlot = plot.id;
                     }
-                    settings.clickHandler(building);
+                    settings.clickHandler(plot);
                     return false;
                 }
             });
@@ -211,7 +212,6 @@
         e = e.originalEvent;
         var delta = e.wheelDelta ? e.wheelDelta / 40 : e.detail ? -e.detail : 0;
         if (delta) {
-            console.log(delta)
             zoom(delta);
         }
         e.preventDefault();
@@ -272,7 +272,7 @@
         return e;
     }
 
-    // Whether or not the device is mobile or not
+    // Whether the device is mobile or not
     function isMobile() {
         return /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0, 4));
     }
@@ -288,7 +288,7 @@
         this.y = y;
         this.width = width;
         this.height = height;
-        this.contains = function (point) {
+        this.contains = function(point) {
             return point.x > this.x && point.x < this.x + this.width && point.y > this.y && point.y < this.y + this.height;
         };
     }
@@ -300,14 +300,15 @@
     }
 
     // Building class
-    function Building(plot, sprite, url = null) {
-        this.plot = plot;
+    function Plot(id, building, sprite, url = null) {
+        this.id = id;
+        this.building = building;
         this.sprite = sprite;
         this.url = url;
         this.render = function (delta) {
             sprite.render(delta);
         };
-        this.click = function () {
+        this.click = function() {
             if (this.url != null) {
                 window.location = this.url;
             }
@@ -327,7 +328,7 @@
             0,
             0
         );
-        this.render = function (delta) {
+        this.render = function(delta) {
             var img = this.hoverImagePath != null && this.hover ? this.hoverImage : this.image;
             if (this.animation != null) {
                 animation.render(delta, img, this.rect);
@@ -345,7 +346,7 @@
         this.frameTime = frameTime;
         this.currentFrame = 0;
         this.currentFrameTime = 0;
-        this.render = function (delta, image, position) {
+        this.render = function(delta, image, position) {
             this.currentFrameTime += delta;
             if (this.currentFrameTime >= this.frameTime) {
                 this.currentFrameTime = 0;
