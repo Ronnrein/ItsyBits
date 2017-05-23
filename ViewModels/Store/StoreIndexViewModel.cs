f5e61d592ee9452aab389fc7c81f721756190d3b
Ronny Reinhardtsen <ronny@ronnrein.com>
2017-05-23 13:59:13 +0200

    Fixed small issues

--- ViewModels/Store/StoreIndexViewModel.cs
+++ ViewModels/Store/StoreIndexViewModel.cs
@@ -3,6 +3,7 @@
 namespace ItsyBits.ViewModels {
     public class StoreIndexViewModel {
         
+        public string Tab { get; set; }
         public IEnumerable<AnimalTypeViewModel> AnimalTypes { get; set; }
         public IEnumerable<BuildingTypeViewModel> BuildingTypes { get; set; }
         public IEnumerable<UpgradeViewModel> Upgrades { get; set; }
