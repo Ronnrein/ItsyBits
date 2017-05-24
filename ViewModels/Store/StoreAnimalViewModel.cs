0f22b133507e2f217ec52bc2bc78f4be9c8a61ca
Ronny Reinhardtsen <ronny@ronnrein.com>
2017-05-24 13:23:18 +0200

    Increased maximum name length for animal and building

--- ViewModels/Store/StoreAnimalViewModel.cs
+++ ViewModels/Store/StoreAnimalViewModel.cs
@@ -6,7 +6,7 @@ namespace ItsyBits.ViewModels {
 
         [Required(ErrorMessage = "Please enter the animals name")]
         [Display(Name = "What should this animal be called?")]
-        [StringLength(10, ErrorMessage = "The name must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
+        [StringLength(20, ErrorMessage = "The name must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
         public string Name { get; set; }
 
         [Required(ErrorMessage = "You have to select a building for the animal")]
